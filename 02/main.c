977dddc393ddc3740c0383032bbdbf73c9c366b3
M6yg6t <prh14231278824947@163.com>
2021-03-30 17:52:17 +0800

    m

---
+++ 02/main.c
@@ -0,0 +1,10 @@
+int main(void){
+	int a,b;
+	printf("enter two numbers:");
+	scanf("%d %d",&a,&b);
+	printf("add:%d\n",add(a,b));
+	printf("sub:%d\n",sub(a,b));
+	printf("mul:%d\n",mul(a,b));
+	printf("div:%lf\n",div(a,b));
+	return 0;
+}
